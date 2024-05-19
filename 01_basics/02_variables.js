907e06cbf5a14b1ffe6702ae8a5c2d5f14779192
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-05-19 10:23:38 +0000

    02 this

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,28 @@
+//const is a keyword
+const acountID = 12345
+//let is also a keyword
+let acountEmail = "shivam13425@google.com"
+//var is a keyword
+var acountPassword = "1234444"
+// you use in case it is correct but not good 
+acountCity = "jaipur"
+// per variable use and not put the value
+let acountState;
+
+//acountID =12342 you can't change const again this is one time diclriation keyword
+console.log(acountID);
+
+acountEmail = "changeemail"
+acountPassword = "changepassword"
+acountCity = "bhopal"
+
+console.table([acountID,acountEmail,acountPassword,acountCity,acountState]);
+
+
+/* note1 :- not use var becaue it have issue in block scope and function scope
+   note2 :- you are not put the value then the vale is undefiend
+   note3 :- const variable is not changeable and most imortant put the valu is compalseri
+   note4 :-
+
+*/
+
---
+++ 01_basics/03_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // it is all code is treat on new version 
+//alert("hello");  we can use node js not browser
+// perttier sre use for code formeting 
+
+
+let myNumber = 123456;
+let myName = "shivam barpete"
+let ha_ya_na = true
+let temp = null
+let js = undefined
+
+
+console.table([myName,myNumber,ha_ya_na,temp,js]);
+
+console.table([typeof(myName),typeof(myNumber),typeof(ha_ya_na),typeof(temp),typeof(js)]);
+
+
+
+
+
+
+
+
+/*                                                  DATA TYPE 
+    01:- Number range aprox 2 to the power 53
+    02:- bitint
+    03:- string
+    04:- boolean
+    05:- null
+    06:- undefined
+    07:- symbol
+
+ */
+
+
