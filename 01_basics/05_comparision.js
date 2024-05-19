050d0d8783fe22049fd9c2e69be70c9d8120d2c2
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-05-19 13:16:13 +0000

    thiss 04

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -21,4 +21,38 @@ console.log(undefined==0);
 
 
 
+/*  
+        ----------------memory--------------
+        1. stack  (primitive) (copy) 
+        2. heap   non premitive refrence
+
+
+*/
+
+let name = "shivam barpete";
+let name1 = name;
+console.log(name);
+console.log(name1);
+
+name1 = "ram ram";
+console.log(name);
+console.log(name1);
+
+
+
+let user1= {
+    nam :"shivam",
+    age: 46,
+
+}
+let user2 = user1;
+console.log(user1.age);
+console.log(user2.age);
+
+user2.age = 66;
+console.log(user1.age);
+console.log(user2.age);
+
+
+
 
---
+++ 01_basics/06_string.js
@@ -0,0 +1,19 @@
+// string normal 
+let name = "shivam";
+let repo = 50;
+console.log(name+repo+"hii");
+// it is not good prectic
+
+// string intpoletion 
+
+console.log(`hii my name is ${name} barpete and my repo cpount ${repo}`);//it is good and best prectice intopolation
+
+// new string syntex
+
+let gameneme = new String('shivam');
+
+console.log(gameneme);
+console.log(gameneme.length);
+console.log(gameneme.toUpperCase());
+console.log(gameneme.charAt(2));
+
