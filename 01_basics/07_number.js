4948949ce9a09f8dff7fbc94f23ec41800a67238
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-07-04 11:50:52 +0000

    08

---
+++ 01_basics/07_number.js
@@ -0,0 +1,22 @@
+console.log(`hii number`);
+//normal dicleretion
+const pin = 100;
+console.log(pin);
+
+//this is retunr ylike full array
+const count = new Number(1000);
+console.log(count);
+
+//change number to string 
+console.log(count.toString());
+
+//get a length a number
+console.log(count.toString().length);
+
+let dacimalvale = 10000.345544;
+console.log(dacimalvale.toFixed(2));
+
+// precision on my numbeer
+console.log(dacimalvale.toPrecision(5
+));
+
