a34a58519ce3ce46c4c04557d31b1da5af6744f6
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-07-05 12:07:02 +0000

    15 commint

---
+++ 01_basics2/array.js
@@ -0,0 +1,48 @@
+// array is the data structure
+
+const myarr = [0,1,2,3,4,5];
+const myheros = ["thor","ironman"];
+const newarry = new Array(1,2,3,4);
+
+
+// console.log(myarr);
+// console.log(myheros);
+// console.log(newarry);
+
+// //size funcation mathode
+// console.log(myarr.length);
+// console.log(myheros.length);
+// console.log(newarry.length);
+
+// console.log(myarr[3]);
+
+//method in array
+
+// myarr.push(1);
+// myarr.pop();
+
+// myarr.unshift(3);
+
+// myarr.shift();
+
+//include method
+
+
+// console.log(myarr.includes(40));
+// console.log(myarr.indexOf(4));
+
+
+// console.log("A",myarr);
+
+// //slice
+
+// const n1arr = myarr.slice(1,3);
+
+// console.log(n1arr);
+// console.log("B",myarr);
+
+// const n2arr = myarr.splice(1,3);
+
+// console.log(n2arr);
+// console.log("C",myarr);
+
---
+++ 01_basics2/array2.js
@@ -0,0 +1,16 @@
+// const marval = ["a","b","c","d","e"];
+// const dchero = ["w","x","y","z"];
+// const indian = ["s", "r", "t"]
+
+// // marval.push(dchero);
+
+// // console.log(marval);
+
+// // const allhero = marval.concat(dchero);
+// // console.log(allhero);
+
+// const allhero = [...marval, ...dchero, ...indian];
+// console.log(allhero);
+
+console.log(Array.isArray("shivam"));
+console.log(Array.from("shivam"));
