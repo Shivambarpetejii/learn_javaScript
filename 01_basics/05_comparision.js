0186fd48ca110050c83932446d8bf733851635e3
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-07-26 06:22:43 +0000

    the conversion memory and stack and heap

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -22,9 +22,22 @@ console.log(undefined==0);
 
 
 /*  
-        ----------------memory--------------
-        1. stack  (primitive) (copy) 
-        2. heap   non premitive refrence
+        ----------------datatypes--------------
+        
+        primitive datatype (call by value)
+           1. number 
+           2.string
+           3.boolean
+           4.Null
+           5.symbole
+           6.undefined
+           7.bigint
+        
+        Non-primitive (call by refrence)
+           1. array
+           2.object
+           3function 
+
 
 
 */
@@ -52,6 +65,44 @@ console.log(user2.age);
 user2.age = 66;
 console.log(user1.age);
 console.log(user2.age);
+
+/* **************memory*****************
+
+//         1. stack  (primitive) (copy) 
+//         2. heap   non premitive refrence
+
+    
+*/
+
+
+let var1 = 123;
+let var2 = var1;
+
+console.log(var1);
+console.log(var2);
+// change the valu 
+var2 = 12333;
+
+console.log(var1);
+console.log(var2);
+// example pass by value
+
+let hero = {
+   heroname: "ram",
+   hero2name: "krishna",
+   hero3name: "lakshman"
+}
+
+let hero2 = hero;
+
+console.log(hero);
+console.log(hero2);
+
+//change 
+hero2.heroname ="ram ram";
+console.log(hero);
+console.log(hero2);
+//example call by value
 
 
 
