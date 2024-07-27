2089572385bd0e1fe0f85a48b82e41fd6ac4d075
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-07-27 11:42:28 +0000

    if steatement

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,69 @@
+// contron flow -- logic flow 
+
+// //step no -1 if 
+// if(true)
+// {
+//     console.log(`i am if statement`);
+// }
+
+
+// //step no-3 if and else
+
+// if(false)
+//     {
+//         console.log(`i am if statement`);
+//     }
+// else{
+//     console.log(`i am else statement`);
+// }
+
+// //step no-3 if,else-if and if
+
+// if(false)
+//     {
+//         console.log(`i am if statement`);
+//     }
+
+// else if(true)
+// {
+//     console.log(`i am else if here`);
+// }
+// else{
+//     console.log(`i am else statement`);
+// }
+
+
+// //step no-6 sorteen notetion 
+
+// if(2<4) console.log(`i am one line notetion`);
+
+
+// //step no-7 AND statement 
+
+// let js = true;
+// let html = true;
+// let css = false;
+
+// if(js && html && css)
+// {
+//     console.log(`yes you are ealigabli`);
+// }
+// else{
+//     console.log(`not good fit`);
+// }
+
+
+
+// //step no-8 OR steatement
+// let js = true;
+// let html = true;
+// let css = false;
+
+// if(js || html || css)
+// {
+//     console.log(`yes you are ealigabli`);
+// }
+// else{
+//     console.log(`not good fit`);
+// }
+
