8197ef56cab4fc4d4117339df2ec60f061192bf8
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2024-07-29 04:26:51 +0000

    ok

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+
+
+// switch statement 
+
+let month = 14;
+
+switch (month) {
+    case 1:
+        console.log(` jan`);
+        break;
+
+    case 2:
+        console.log(`fab`);
+        break;
+    case 3:
+        console.log(`mar`);
+        break;
+    case 4:
+        console.log(`apr`);
+        break;
+
+    default:
+        console.log(`no!!`);
+        break;
+}
---
+++ 04_control_flow/truthy_falsy.js
@@ -0,0 +1,71 @@
+//step no-1 whose value is truthy and falsy
+
+// const useremail = "barpetejii@hmail.com";
+
+// if(useremail)
+// {
+//     console.log(`user email i got it`);
+// }
+// else{
+//     console.log(`not found`);
+// }
+
+
+// const useremail = "";
+
+// if(useremail)
+// {
+//     console.log(`user email i got it`);
+// }
+// else{
+//     console.log(`not found`);
+// }
+
+// //falsy value ---- ""  0  -0  false  bigint0n  null undifined NaN
+// // truthy vallue ---- :"0"  "false"  " "  [] {} funcation 
+
+
+// //step no-2 arrys is empty
+
+// const arr = [];
+
+// if(arr.length === 0)
+// {
+//     console.log(`yes is empty`);
+// }
+// else
+// {
+//     console.log(`not empty`);
+// }
+
+
+// // step no-3 object is empty
+
+// const obj ={};
+
+// if(Object.keys(obj).length === 0)
+// {
+//     console.log(`yes is empty`);
+
+// }
+// else
+// {
+//     console.log(`not empty`);
+// }
+
+
+// // step no-4 nullish coalescing operator(??)
+
+// // let a = 5 ?? 10
+// // let a = null ?? 10
+// let a = undefined ?? 10
+
+// console.log(a);
+
+
+//step no -5  terniary operaotr
+
+let val = 10;
+
+val >= 6 ? console.log(`this is greter then 6`):console.log(`this is less then 6`);
+
